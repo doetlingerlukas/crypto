88c637579a2055eb08089c49451a84448df74d07
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2018-06-07 01:54:25 +0200

    Turbofish decode.

--- shift_chiffre-rs/src/main.rs
+++ shift_chiffre-rs/src/main.rs
@@ -3,8 +3,6 @@ fn main() {
   let a = 'A' as u8;
 
   for i in 0..26 {
-    let decoded_text: String =
-      chiffre_text.chars().map(|c| ((c as u8 - a + i) % 26 + a) as char).collect();
-    println!("{}", decoded_text);
+    println!("{}", chiffre_text.chars().map(|c| ((c as u8 - a + i) % 26 + a) as char).collect::<String>());
   }
 }
