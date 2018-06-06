19857c7fe30b50a0835a97d6103221fec397c8e2
Markus Reiter <me@reitermark.us>
2018-06-07 01:57:35 +0200

    Refactor variables.

--- shift_chiffre-rs/src/main.rs
+++ shift_chiffre-rs/src/main.rs
@@ -1,8 +1,7 @@
 fn main() {
-  let chiffre_text = "UJGUGNNUUGCUJGNNUDAVJGUGCUJQTG";
-  let a = 'A' as u8;
-
   for i in 0..26 {
-    println!("{}", chiffre_text.chars().map(|c| ((c as u8 - a + i) % 26 + a) as char).collect::<String>());
+    println!("{}", "UJGUGNNUUGCUJGNNUDAVJGUGCUJQTG".chars()
+      .map(|c| ((c as u8 - 'A' as u8 + i) % 26 + 'A' as u8) as char)
+      .collect::<String>());
   }
 }
