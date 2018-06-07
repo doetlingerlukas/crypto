e568998a39a970073c84ed998bfcdbce65629893
Markus Reiter <me@reitermark.us>
2018-06-07 03:00:44 +0200

    Subtract index in Rust version.

--- shift_chiffre-rs/src/main.rs
+++ shift_chiffre-rs/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
   for i in 0..26 {
-    println!("{}", "UJGUGNNUUGCUJGNNUDAVJGUGCUJQTG".chars()
-      .map(|c| ((c as u8 - 'A' as u8 + i) % 26 + 'A' as u8) as char)
-      .collect::<String>());
+    println!("{} {}", "UJGUGNNUUGCUJGNNUDAVJGUGCUJQTG".chars()
+      .map(|c| ((c as u8 - 'A' as u8 + 26 - i) % 26 + 'A' as u8) as char)
+      .collect::<String>(), i);
   }
 }
